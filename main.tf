b88d08a96717fd9e5d22d81ac73e5d275caf24a3
cindyhu-gd <cindy.hu@greendotcorp.com>
2022-03-04 18:41:00 +0800

    create storage account

--- main.tf
+++ main.tf
@@ -5,12 +5,6 @@ terraform {
       source  = "hashicorp/azurerm"
     }
   }
-  backend "azurerm" {
-    resource_group_name   = "jonnychipz-state"
-    storage_account_name  = "jonnychipztfstate"
-    container_name        = "tstate"
-    key                   = "terraform.tfstate"
-    }
 }
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
@@ -18,23 +12,19 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "myrg" {
-  name     = "jonnychipz-rg"
+resource "azurerm_resource_group" "devsandboxrg" {
+  name     = "SOFTWARE-DEV-SANDBOX-RESTRICTED-RG"
   location = var.primary_location
 }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "myvnet" {
-  name                = "jonnychipz-vnet"
-  resource_group_name = azurerm_resource_group.myrg.name
-  location            = azurerm_resource_group.myrg.location
-  address_space       = ["10.0.0.0/16"]
-}
+resource "azurerm_storage_account" "cindysa" {
+  name                     = "cindyteststorageaccount"
+  resource_group_name      = azurerm_resource_group.devsandboxrg.name
+  location                 = azurerm_resource_group.devsandboxrg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-# Create a subnet within the vnet
-resource "azurerm_subnet" "mysubnet" {
-  name                 = "jonnychipz-subnet"
-  resource_group_name  = azurerm_resource_group.myrg.name
-  virtual_network_name = azurerm_virtual_network.myvnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  tags = {
+    BusinessOwner = "Cindy Hu"
+  }
 }
